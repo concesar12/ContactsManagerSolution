fb6bcb3fa3950e247db917f04768fa8038dc374b
Cesar David Guerrero Regino <concesar12@gmail.com>
2023-06-25 19:19:53 +1200

    12. Remote Validation

--- ContactsManager.UI/Controllers/AccountController.cs
+++ ContactsManager.UI/Controllers/AccountController.cs
@@ -94,5 +94,18 @@ namespace ContactsManager.UI.Controllers
             await _signInManager.SignOutAsync();
             return RedirectToAction(nameof(PersonsController.Index), "Persons");
         }
+
+        public async Task<IActionResult> IsEmailAlreadyRegistered(string email)
+        {
+            ApplicationUser user = await _userManager.FindByEmailAsync(email);
+            if (user == null)
+            {
+                return Json(true); //valid json value that is expected from the browser
+            }
+            else
+            {
+                return Json(false); //invalid
+            }
+        }
     }
 }
